4c174da7ab9b8d04036482f61acaaa987724f2cc
Lillie Schachter <lillieschachter@gmail.com>
2022-02-08 17:11:39 -0500

    update week 2 challenges

--- challenges/04_print_comments_variables/first_python_challenge.py
+++ challenges/04_print_comments_variables/first_python_challenge.py
@@ -9,15 +9,11 @@ print('1.3: Update the meal variable to be a string describing what you want for
 
 print('2: How old is Google?')
 # 2.1: Google was founded in 1993. The current year is 2022. Create a variable called google_age, and use subraction
-# to figure out how old Google is
+# to figure out how old Google is. Then print it out
 # ex: my_age = current_year - birth_year
 
 
-# 2.2: Print out a sentence about Google's age. Make sure to include your variable in the f-string!
-
-
-
-# 2.3 How many _months_ old is Google? Create a new variable google_age_months, and use multiplication to figure it out,
+# 2.2 How many _months_ old is Google? Create a new variable google_age_months, and use multiplication to figure it out,
 # then print the info.
 # (Assume 12 months for each year, you don't need to check which month they started)
 
@@ -27,7 +23,9 @@ print('3.1: The line of code below is commented out because it produces many Syn
 print('Fix the problem and turn the comment back into regular Python code')
 #completion message = 'Completed the first Python challenge!
 
+
 # 3.2 What were the syntax errors that you fixed? print out a quick explanation of each one.
+
 
 print('3.3: Turn the comment below back into regular Python code')
 #print(completion_message)
