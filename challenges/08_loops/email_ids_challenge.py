ba1aa3dc1f74444f4cd55c84da4e2d5757ece28d
Lillie Schachter <lillieschachter@gmail.com>
2022-02-14 17:29:34 -0500

    swap loops and dictionaries

---
+++ challenges/08_loops/email_ids_challenge.py
@@ -0,0 +1,12 @@
+print('Question 1')
+
+# Congratulations! Your team is expanding. Below is a list of their names:
+names = ['Max Bartlett', 'Angelita Norris', 'Stewart Mueller', 'Dominique Henry', 'Carmela Gross', 'Bettie Mcmillan', 'Sara Ellison', 'Ira Anthony', 'Pauline Riley', 'Ben Weber',
+         'Joanne Mcknight', 'Loren Gould', 'Jamar Singh', 'Amanda Vance', 'Tyrell Andrade', 'Jana Clements', 'Eddy Mcbride', 'Marsha Meyer', 'Elbert Shannon', 'Alyce Hull']
+
+emails = []
+
+# We want to convert all their names into the same format from Question 1
+# 2.1 TODO: Use a "for" loop to go over each name in the names list
+# 2.2 TODO: Inside the "for" loop, create the email id by re-using the logic from Question 1 and...
+# 2.3 TODO: ..add the email to the emails list
